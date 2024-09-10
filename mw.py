c23b5962da0128feee9a5ae05d61f3e3379b88aa
SysAdminSecure <carlos.armenta.soto@gmail.com>
2024-09-09 17:30:27 -0700

    License implementation

--- mw.py
+++ mw.py
@@ -1,3 +1,29 @@
+'''
+  COPYRIGHT DISCLAIMER
+
+Script: Malwarepy or mw.py - Malware simulator for internal audits
+
+Copyright (C) 2024 https://github.com/SysAdminSecure
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version, with the following restrictions:
+
+1. **Credits:** Credit must be given to the original author, https://github.com/SysAdminSecure, on any redistribution or modification of the script.
+In addition, any monetary gratuity is appreciated as optional acknowledgment.
+
+2. **Copying and Permissions:** Copying the code in its entirety is not permitted without explicit permission from the author.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; even without the implied warranty of MERCHANTABILITY or 
+FITNESS FOR A PARTICULAR PURPOSE. No responsibility is assumed for the use of malware generated with this script, whether for legitimate purposes or not.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+For any questions, see: https://github.com/SysAdminSecure.
+
+'''
+
+
 ## MalWare.PYdd
 import os  
 from complemets import  Banner, color
